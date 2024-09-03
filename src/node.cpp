f31bbb6b2b3242905a5467e16a353a576d5a8165
wsribunm <worawissribunma@gmail.com>
2024-09-02 21:22:36 -0400

    add joy2serial

--- src/node.cpp
+++ src/node.cpp
@@ -6,41 +6,74 @@
 #include <boost/asio.hpp> 
 using namespace boost;
 using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+
+union servo_data_t{
+  char bytes[12];
+  uint16_t data[6];
+};
 
 class MinimalSubscriber : public rclcpp::Node
-
 {
   public:
+    //Public member method
     MinimalSubscriber()
-    : Node("minimal_subscriber"), port(io)
+    : Node("minimal_subscriber"), m_io(), m_port(m_io)
     {
-      subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
+      m_subscription = this->create_subscription<sensor_msgs::msg::Joy>(
       "joy", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
 
-      port.open("/dev/ttyACM0");
-      port.set_option(asio::serial_port_base::baud_rate(115200));
-      
+      auto get_status =
+      [this]() -> void
+      {
+        memset(m_out_buf, 0, 2048);
+        size_t n_read = m_port.read_some(asio::buffer(m_out_buf,2048));
+        m_out_buf[n_read] = '\0';
+        RCLCPP_INFO(this->get_logger(), "%s", m_out_buf);
+      };
+      // m_timer  = this->create_wall_timer(1s, get_status);
+
+      m_port.open("/dev/ttyACM0");
+      m_port.set_option(asio::serial_port_base::baud_rate(115200));
+      for (int i = 0; i<5; i++){
+        m_servo_data.data[i] = 1000;
+      }      
     }
     ~MinimalSubscriber(){
-        port.close();
+        m_port.close();
     }
+    //public member attributes
 
   private:
-    void topic_callback(const sensor_msgs::msg::Joy::SharedPtr msg) const
+    // Private member methods
+    void topic_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
     {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->axes[0]);
-    //   asio::write_some(buf);
+      RCLCPP_INFO(this->get_logger(), "joy: %f %f %f %f",
+        msg->axes[0], msg->axes[1], msg->axes[2], msg->axes[3]);
+      m_servo_data.data[0] = 1000*msg->axes[1] + 1000;
+      m_servo_data.data[1] = 500*msg->axes[3] + 1500;
+      m_servo_data.data[2] = 500*msg->axes[4] + 1500;
+      m_servo_data.data[3] = 500*msg->axes[0] + 1500;
+      m_servo_data.data[4] = 500*msg->axes[2] + 1500;
+      m_servo_data.bytes[10] = '\n';
+      m_port.write_some(asio::buffer(m_servo_data.bytes, 11));
     }
-    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
-    asio::io_service io;
-    asio::serial_port port;
-
+    
+    // Member attributes
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr m_subscription;
+    asio::io_service m_io;
+    asio::serial_port m_port;
+    servo_data_t m_servo_data;
+    u_int8_t m_out_buf[2048];
+    rclcpp::TimerBase::SharedPtr m_timer;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  auto node = std::make_shared<MinimalSubscriber>();
+  rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
 }
